2cf0af105f3bc6cbb83080b0de908be19ed4e0c9
Harshit Sahu <95880196+sahuharshit408@users.noreply.github.com>
2025-08-27 10:58:03 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/scim/config/SecurityConfig.java
+++ src/main/java/com/example/scim/config/SecurityConfig.java
@@ -18,10 +18,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            //.addFilterBefore(new BearerTokenAuthFilter(expectedToken), UsernamePasswordAuthenticationFilter.class)
+            .authorizeHttpRequests(auth -> auth.anyRequest().Authenticated())
+            .addFilterBefore(new BearerTokenAuthFilter(expectedToken), UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 }
+
 
 
