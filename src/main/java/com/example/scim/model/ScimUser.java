276680d8579cbb35f97d422a55fb5f53dc061a4e
Harshit Sahu <95880196+sahuharshit408@users.noreply.github.com>
2025-08-28 18:14:48 +0530

    Update ScimUser.java

--- src/main/java/com/example/scim/model/ScimUser.java
+++ src/main/java/com/example/scim/model/ScimUser.java
@@ -2,10 +2,15 @@ package com.example.scim.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ScimUser {
+
+    private List<String> schemas = List.of("urn:ietf:params:scim:schemas:core:2.0:User");
     private String id;
     private String userName;
     private Boolean active;
     private String displayName;
 }
+
